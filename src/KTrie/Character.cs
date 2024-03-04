e19907e328d08988c5c7afb43723124af4709486
Kirill Polishchuk <kirill.polishchuk@iag.co.nz>
2024-03-05 10:30:58 +1300

    chore: refactoring

---
+++ src/KTrie/Character.cs
@@ -0,0 +1,20 @@
+﻿namespace KTrie;
+
+public class Character(char c)
+{
+    public static Character Any => AnyCharacter.Instance;
+
+    public char Char => c;
+
+    public static implicit operator Character(char c) => new(c);
+
+    private class AnyCharacter : Character
+    {
+        public static readonly AnyCharacter Instance = new();
+
+        private AnyCharacter() : base(char.MinValue)
+        {
+
+        }
+    }
+}
